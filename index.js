1352bd81e7b40f552c8e8efe9e0441513bc824b5
Josh Hrycek <joshhrycek@gmail.com>
2018-08-30 14:40:24 -0400

    Psudocode

---
+++ index.js
@@ -0,0 +1,30 @@
+const COMICVINE_URL = ""
+const GOODREADS_URL = ""
+
+function getDataFromComicvine() {
+
+}
+
+function displayComicvineData() {
+
+}
+
+function getDataFromGoodreads() {
+
+}
+
+function displayGoodreadsData() {
+
+}
+
+function renderIntro() {
+
+}
+
+function renderHeroPage() {
+
+}
+
+function renderStoryPage() {
+
+}
