24ec92e25b2d35a0a58871d1ebdad6d5f64a91ad
Josh Hrycek <joshhrycek@gmail.com>
2018-09-01 13:12:38 -0400

    new docs

---
+++ docs/index.js
@@ -0,0 +1,70 @@
+const COMICVINE_URL = "https://comicvine.gamespot.com/api/characters"
+const GOODREADS_URL = ""
+const AMAZON_URL = ""
+
+function getDataFromComicvine(serachTerm, callback) {
+    const settings = {
+        URL: COMICVINE_URL,
+        data: {
+            api_key : "a13358abcad5c60e3c10a801fdc83a487e50c2a8",
+            filter : `name:${serachTerm}`,
+        },
+        datatype: "jsonp",
+        type: "GET",
+        success: callback
+    };
+    $.ajax(settings);
+}
+
+function displayComicvineData(data) {
+    $('main').html(renderHeroPage(data))
+}
+
+function getDataFromGoodreads() {
+    const query = {
+        
+    }
+    $.getJSON(GOODREADS_URL, query, callback)
+}
+
+function displayGoodreadsData() {
+
+}
+
+function getDataFromAmazon() {
+    const query = {
+        
+    }
+    $.getJSON(AMAZON_URL, query, callback)
+}
+
+function displayAmazonData() {
+
+}
+
+function getSearchTerm() {
+    $('form').on('submit', event => {
+        event.preventDefault();
+        var query = $(".serach-box").val()
+        getDataFromComicvine(query, displayComicvineData)
+    })
+}
+
+function renderHeroPage(data) {
+    return `<h2>${data.results.name}</h2>
+    <p>${data.results.deck}</p>`
+}
+
+function renderStoryPage() {
+
+}
+
+function renderReviewPage() {
+
+}
+
+function createApp() {
+    getSearchTerm()
+}
+
+$(createApp)
